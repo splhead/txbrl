b4b86d47521b82a56dec5feacfb12ccd9e4cb36c
Silas P Ladislau <splhead@gmail.com>
2022-10-07 13:04:19 -0400

    rascunho

--- src/usecases/process-xbrl.usecase.ts
+++ src/usecases/process-xbrl.usecase.ts
@@ -3,10 +3,30 @@ import path from 'path'
 import { XMLParser, XMLBuilder } from 'fast-xml-parser'
 
 class ProcessXBRLUseCase {
+  private eObjeto(valor: unknown): boolean {
+    return valor !== null && !Array.isArray(valor) && typeof valor === 'object'
+  }
+
+  private mesclaObjetos(
+    objeto: Record<string, unknown>,
+    objeto2: Record<string, unknown>
+  ): Record<string, unknown> {
+    Object.assign(objeto, objeto2)
+    return objeto
+  }
+
   public async execute(): Promise<void> {
-    const caminhoDoArquivo = path.join(__dirname, '..', 'template', 'modelo.xml')
+    const caminhoDoArquivo = path.join(
+      __dirname,
+      '..',
+      'template',
+      'modelo.xml'
+    )
     fs.readFile(caminhoDoArquivo, (erro, dados) => {
-      const tradutorXML = new XMLParser({ ignoreAttributes: false, ignoreDeclaration: true })
+      const tradutorXML = new XMLParser({
+        ignoreAttributes: false,
+        ignoreDeclaration: true
+      })
       const construtorXML = new XMLBuilder({ ignoreAttributes: false })
       const objetoXML = tradutorXML.parse(dados)
       //console.log(objetoXML, '\n')
@@ -19,21 +39,30 @@ class ProcessXBRLUseCase {
 
       const arrayXBRL = Object.entries(objetoXML[xbrlTag])
 
-      const xbrlAttributos = arrayXBRL.filter(linha => linha[0].includes('@_'))
+      // obtem os atributos
+      const xbrlAttributos = arrayXBRL.filter((posicao) =>
+        posicao[0].startsWith('@_')
+      )
 
       // adiciona os atributos
-      xbrlAttributos.forEach(linha => {
-        Object.assign(xbrlElemento[xbrlTag], { [linha[0]]: linha[1] })
+      xbrlAttributos.forEach((posicao) => {
+        Object.assign(xbrlElemento[xbrlTag], { [posicao[0]]: posicao[1] })
       })
 
-      const xbrlFilhos = arrayXBRL.filter(linha => !linha[0].includes('@_'))
+      const xbrlFilhos = arrayXBRL.filter(
+        (posicao) => !posicao[0].startsWith('@_')
+      )
 
-      xbrlFilhos.forEach(linha => {
-        Object.assign(xbrlElemento[xbrlTag], { [linha[0]]: linha[1]})
+      xbrlFilhos.forEach((posicao) => {
+        console.log({ [posicao[0]]: posicao[1] }, this.eObjeto(posicao[1]))
+        if (posicao[0] === 'gl-cor:accountingEntries') {
+          Object.assign(xbrlElemento[xbrlTag], { [posicao[0]]: posicao[1] })
+        }
       })
 
-      const saida = construtorXML.build(xbrlElemento)
-      console.log(saida)
+      console.log('\n\n\n', xbrlElemento)
+      //const saida = construtorXML.build(xbrlElemento)
+      //console.log(saida)
     })
   }
 }
